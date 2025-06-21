0cf629e74276fab5595a7718c3432304bdbaa037
ryomensukuna2003 <mshivanshu1264@gmail.com>
2025-06-21 20:35:49 +0530

    Now backend is using Google gemini API

--- server/services/ragService.js
+++ server/services/ragService.js
@@ -1,10 +1,13 @@
 import { QdrantVectorStore } from "@langchain/qdrant";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
-import { OllamaEmbeddings } from "@langchain/ollama";
-import { ChatOllama } from "@langchain/ollama";
+import {
+  GoogleGenerativeAIEmbeddings,
+  ChatGoogleGenerativeAI,
+} from "@langchain/google-genai";
 import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
 import { createRetrievalChain } from "langchain/chains/retrieval";
 import { ChatPromptTemplate } from "@langchain/core/prompts";
+import "dotenv/config";
 
 let vectorStore;
 
@@ -17,7 +20,10 @@ export const embedAndStoreChunks = async (rawText, docName) => {
   const chunks = await splitter.splitText(rawText);
   const metadatas = chunks.map((_, i) => ({ doc: docName, index: i }));
 
-  const embeddings = new OllamaEmbeddings({ model: "nomic-embed-text" });
+  const embeddings = new GoogleGenerativeAIEmbeddings({
+    model: "embedding-001",
+    apiKey: process.env.GOOGLE_API_KEY,
+  });
 
   vectorStore = await QdrantVectorStore.fromTexts(
     chunks,
@@ -29,12 +35,16 @@ export const embedAndStoreChunks = async (rawText, docName) => {
     }
   );
 };
+
 export const getAnswerFromChunks = async (question) => {
   if (!vectorStore) {
-  throw new Error("Vector store not initialized. Upload a document first.");
-}
+    throw new Error("Vector store not initialized. Upload a document first.");
+  }
 
-  const model = new ChatOllama({ model: "mistral" });
+  const model = new ChatGoogleGenerativeAI({
+    model: "gemini-2.0-flash",
+    apiKey: process.env.GOOGLE_API_KEY,
+  });
 
   const prompt = ChatPromptTemplate.fromTemplate(
     `Use the following documents to answer the question:
